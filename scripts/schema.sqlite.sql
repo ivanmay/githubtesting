77f68b7d80baf1ba0512f63c70c8bd38c52b562d
Ivan May <ivan.d.may@gmail.com>
2016-04-07 11:11:27 +0200

    Initial Commit

---
+++ scripts/schema.sqlite.sql
@@ -0,0 +1,15 @@
+
+/**
+ * Created by PhpStorm.
+ * User: ivanmay
+ * Date: 2016/04/06
+ */
+
+CREATE TABLE guestbook (
+id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(32) NOT NULL DEFAULT 'noemail@test.com',
+    comment TEXT NULL,
+    created DATETIME NOT NULL
+);
+
+CREATE INDEX "id" ON "guestbook" ("id");
